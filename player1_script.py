35e26e06055f1feb29e7faaf3f23aedbfaad8d32
Liam-Weitzel <liam.weitzel@gmail.com>
2024-10-06 21:22:26 +0200

    Orchistrator using stdout, and one process per player

--- player1_script.py
+++ player1_script.py
@@ -1,3 +1,13 @@
 import sys
-game_state = sys.stdin.read()
-print("ATTACK")
+
+while True:
+    game_state = sys.stdin.readline().strip()
+    action = "??"
+
+    if int(game_state)%2==0:
+        action = "ATTACK"
+    else:
+        action = "DEFEND"
+
+    print(action)
+    sys.stdout.flush()
--- player2_script.cpp
+++ player2_script.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char *argv[]) {
+int main() {
     std::string game_state;
-    std::getline(std::cin, game_state);
-
-    std::string action = "MOVE_RIGHT";
-
-    std::cout << action << std::endl;
+    
+    while (std::getline(std::cin, game_state)) {
+        std::string action;
+        std::stoi(game_state)%2 == 0 ? action = "MOVE_RIGHT" : action = "MOVE_LEFT";
+        std::cout << action << std::endl;
+    }
 
     return 0;
 }
