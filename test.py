568b76939c83d05ec622ffedb9b51d4b55daf1a3
Tabo <taboweitzel@gmail.com>
2024-10-09 00:34:43 +0200

    WIP initialized game state

--- test.py
+++ test.py
@@ -2,6 +2,10 @@ import numpy as np
 import pandas as pd
 from shapely.geometry.point import Point
 import shapely.affinity
+from descartes import PolygonPatch
+import shapely.affinity
+import matplotlib.pyplot as plt
+
 
 #Dummy player data (should be fetched from DB)
 player1_units = ["ant","ant"]
@@ -12,9 +16,11 @@ map_size = [1080,1080]
 level_id = 7
 
 #Load unit attributes from somewhere (to be determined)
-unit_db = {"ant":{"max_hp":100,"shape":{"type":"ellipse","width":2,"height":7}}}
+unit_db = {"ant":{"max_hp":100}}
 
 #Initialize game state
+#d = ["unit_id": np.nan, "player": np.nan, "unit_health":np.nan, "past_action":np.nan, "lock_out_ticks":np.nan, "unit_hitbox": np.nan, "location_x":np.nan, "location_y":np.nan, "orientation":np.nan, "shape":np.nan]
+
 unit = {"id":"","type":"","player":"","unit_health":"","past_action":"","lock_out_ticks":"","location_x":"","location_y":"","orientation":"","shape":""}
 
 game_state = {"units":[]}
@@ -30,8 +36,23 @@ for entity in player1_units:
     unit["location_x"] = 100
     unit["location_y"] = (1080 / len(player1_units)+2) * i+1
     unit["orientation"] = 90
-    unit["shape"] = unit_db[entity]["shape"]
 
-    game_state["units"].append(unit)
 
     i+=1
+
+
+ellipse = ((0, 0),(2, 7))
+# Let create a circle of radius 1 around center point:
+circ = shapely.geometry.Point(ellipse[0]).buffer(1)
+# Let create the ellipse along x and y:
+ell  = shapely.affinity.scale(circ, int(ellipse[1][0]), int(ellipse[1][1]))
+
+
+from shapely.geometry import Polygon
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = [7.00, 3.50]
+plt.rcParams["figure.autolayout"] = True
+x, y = ell.exterior.xy
+plt.plot(x, y, c="red")
+plt.show()
