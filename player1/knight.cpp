d746885f566afd937ca5669a8357c65356f00c3f
Liam-Weitzel <liam.weitzel@gmail.com>
2024-10-13 02:52:43 +0200

    pipe init_game_status to player processes

--- player1/knight.cpp
+++ player1/knight.cpp
@@ -2,15 +2,16 @@
 #include <string>
 #include <tuple>
 #include <unordered_map>
+#include "../json.hpp"
 
-std::tuple<int, int, float, std::string> knight_main(const std::string& unit_state, std::unordered_map<std::string, std::string>& memory) {
+std::tuple<int, int, float, std::string> knight_main(const nlohmann::json& unit_state, std::unordered_map<std::string, std::string>& memory) {
     int x_direction = 1; // Direction_x and direction_y dictate which direction your action will be executed in.
     int y_direction = 0; // The sum of direction x and y cannot be greater than 1 and not lower than -1.
     float velocity = 0.5f; // Only required for move and skill shots
     std::string action = "move"; // Possible actions: move, attack, block
 
     // Your code goes here
-    std::cout << "test2" << std::endl;
+    // std::cout << "running from cpp" << std::endl;
 
     return std::make_tuple(x_direction, y_direction, velocity, action);
 }
--- player_code_runner.cpp
+++ player_code_runner.cpp
@@ -1,21 +1,26 @@
+#include <cassert>
 #include <iostream>
 #include <string>
 #include <unordered_map>
 
-#include "player2/knight.h"
+#include "json.hpp"
+#include "player1/knight.h"
 
 int main(int argc, char** argv) {
-    std::string unit_state;
-    std::string unit_id;
+    std::string unit_state_str;
+    int unit_id;
     std::string player_number = argv[1];
+    nlohmann::json unit_state;
     
-    std::unordered_map<std::string, std::unordered_map<std::string, std::string>> memory;
+    std::unordered_map<std::string, std::unordered_map<std::string, std::string>> memory = std::unordered_map<std::string, std::unordered_map<std::string, std::string>>();
     memory["unit1"] = std::unordered_map<std::string, std::string>();
     memory["unit2"] = std::unordered_map<std::string, std::string>();
     memory["unit3"] = std::unordered_map<std::string, std::string>();
 
-    while (std::getline(std::cin, unit_state)) {
-        unit_id = unit_state;
+    while (std::getline(std::cin, unit_state_str)) {
+
+        unit_state = nlohmann::json::parse(unit_state_str);
+        unit_id = unit_state["units"][0]["id"];
 
         // TODO: Recognize what unit I am from unit_state, call the correct function
 
