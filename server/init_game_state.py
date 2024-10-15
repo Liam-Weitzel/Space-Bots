f0fa134b9f22727035c61b7d7945d5812c7f5ce1
Liam-Weitzel <liam.weitzel@gmail.com>
2024-10-15 21:17:13 +0200

    Add procgen in game state & render on client, work on unit db

--- server/init_game_state.py
+++ server/init_game_state.py
@@ -1,52 +1,336 @@
-def main():
-    #Dummy player data (should be fetched from DB)
-    player_units = [["acid_ant","acid_ant","rhino_beetle"], ["lava_ant","lava_ant", "mantis"]]
+import random
 
-    #Dummy_game_settings
+def check_collision(rock, existing_rocks):
+    for existing_rock in existing_rocks:
+        left1, right1 = rock["location_x"], rock["location_x"] + rock["shape"]["width"]
+        top1, bottom1 = rock["location_y"], rock["location_y"] + rock["shape"]["height"]
+
+        left2, right2 = existing_rock["location_x"], existing_rock["location_x"] + existing_rock["shape"]["width"]
+        top2, bottom2 = existing_rock["location_y"], existing_rock["location_y"] + existing_rock["shape"]["height"]
+
+        if left1 < right2 and right1 > left2 and top1 < bottom2 and bottom1 > top2:
+            return True
+    return False
+
+def check_unit_collision(rock, units):
+    for unit in units:
+        left1, right1 = rock["location_x"], rock["location_x"] + rock["shape"]["width"]
+        top1, bottom1 = rock["location_y"], rock["location_y"] + rock["shape"]["height"]
+
+        left2, right2 = unit["location_x"], unit["location_x"] + unit["shape"]["sprite"]
+        top2, bottom2 = unit["location_y"], unit["location_y"] + unit["shape"]["sprite"]
+
+        if left1 < right2 and right1 > left2 and top1 < bottom2 and bottom1 > top2:
+            return True
+    return False
+
+def generate_terrain(map_size, units):
+    rock_types = {
+        "rocks_48x48": {"type": "rectangle", "width": 48, "height": 48, "variations": 8},
+        "rocks_48x96": {"type": "rectangle", "width": 48, "height": 96, "variations": 4},
+        "rocks_96x96": {"type": "rectangle", "width": 96, "height": 96, "variations": 3}
+    }
+
+    formations = [
+        {   # Concave shape made up of 6 large rocks
+            "name": "concave",
+            "rocks": [
+                {"type": "rocks_96x96", "offset_x": 0, "offset_y": 0},
+                {"type": "rocks_96x96", "offset_x": 96, "offset_y": 0},
+                {"type": "rocks_96x96", "offset_x": 192, "offset_y": 0},
+                {"type": "rocks_96x96", "offset_x": 48, "offset_y": 96},
+                {"type": "rocks_96x96", "offset_x": 144, "offset_y": 96},
+                {"type": "rocks_96x96", "offset_x": 96, "offset_y": 192}
+            ]
+        },
+        {   # L shape formation
+            "name": "l_shape",
+            "rocks": [
+                {"type": "rocks_48x96", "offset_x": 0, "offset_y": 0},
+                {"type": "rocks_48x96", "offset_x": 0, "offset_y": 96},
+                {"type": "rocks_48x96", "offset_x": 0, "offset_y": 192},
+                {"type": "rocks_48x96", "offset_x": 48, "offset_y": 192},
+                {"type": "rocks_48x48", "offset_x": 48, "offset_y": 240}
+            ]
+        },
+        {   # Random cluster formation
+            "name": "cluster",
+            "rocks": [
+                {"type": "rocks_48x48", "offset_x": 0, "offset_y": 0},
+                {"type": "rocks_96x96", "offset_x": 96, "offset_y": 48},
+                {"type": "rocks_48x96", "offset_x": -48, "offset_y": 48},
+                {"type": "rocks_48x48", "offset_x": 48, "offset_y": 144}
+            ]
+        }
+    ]
+
+    terrain = []
+    half_map_width = map_size[0] // 2
+    num_formations = 3
+
+    for _ in range(num_formations):
+        placed_successfully = False
+        while not placed_successfully:
+            formation = random.choice(formations)
+
+            # Generate random position on the left half of the map
+            location_x = random.randint(0, half_map_width - 200)
+            location_y = random.randint(0, map_size[1] - 200)
+
+            new_rocks = []
+
+            # Try adding each rock in the formation to the temporary list
+            collision_detected = False
+            for rock in formation["rocks"]:
+                rock_type = rock["type"]
+                rock_variation = random.randint(0, rock_types[rock_type]["variations"] - 1)
+
+                left_rock = {
+                    "type": rock_type,
+                    "variation": rock_variation,
+                    "location_x": location_x + rock["offset_x"],
+                    "location_y": location_y + rock["offset_y"],
+                    "shape": {
+                        "type": rock_types[rock_type]["type"],
+                        "width": rock_types[rock_type]["width"],
+                        "height": rock_types[rock_type]["height"]
+                    },
+                    "orientation": "left"
+                }
+
+                if check_collision(left_rock, terrain) or check_unit_collision(left_rock, units):
+                    collision_detected = True
+                    break
+
+                new_rocks.append(left_rock)
+
+                # Mirror the formation on the right side
+                right_rock = {
+                    "type": rock_type,
+                    "variation": rock_variation,
+                    "location_x": map_size[0] - (location_x + rock["offset_x"]),
+                    "location_y": location_y + rock["offset_y"],
+                    "shape": left_rock["shape"],
+                    "orientation": "right"
+                }
+
+                if check_collision(right_rock, terrain) or check_unit_collision(right_rock, units):
+                    collision_detected = True #NOTE: Not really needed as its already done on left side
+                    break
+
+                new_rocks.append(right_rock)
+
+            if not collision_detected:
+                terrain.extend(new_rocks)
+                placed_successfully = True
+
+    return terrain
+
+def main():
+    player_units = [["acid_ant", "acid_ant", "rhino_beetle", "bloated_bedbug", "dung_beetle"],
+                    ["lava_ant", "lava_ant", "mantis", "foraging_maggot"]]
     map_size = [1080,1080]
-    level_id = 7
 
     #Load unit attributes from somewhere (to be determined)
     unit_db = {
-            "acid_ant":{
-                "max_hp":100,
-                "shape":{
-                    "type":"ellipse",
-                    "width":2,
-                    "height":7
-                    },
-                "mass":5
-                },
-            "lava_ant":{
-                "max_hp":200,
-                "shape":{
-                    "type":"ellipse",
-                    "width":2,
-                    "height":7
-                    },
-                "mass":5
-                },
-            "rhino_beetle":{
-                "max_hp":150,
+            "acid_ant":{ #Your standard soldier
+                "max_hp":70,
+                "damage":15,
+                "attack_speed":2,
+                "move_speed":2,
                 "shape":{
                     "type":"ellipse",
                     "width":5,
-                    "height":7
+                    "height":2,
+                    "sprite": 48
+                    },
+                "mass":5
+                },
+            "bloated_bedbug":{ #Medium price, high alpha
+                "max_hp":20,
+                "damage":50,
+                "attack_speed":4,
+                "move_speed":2,
+                "shape":{
+                    "type":"ellipse",
+                    "width":5,
+                    "height":2,
+                    "sprite": 48
+                    },
+                "mass":3
+                },
+            "dung_beetle":{ #Pusher, slow, tanky
+                "max_hp":200,
+                "damage":5,
+                "attack_speed":2,
+                "move_speed":0.5,
+                "shape":{
+                    "type":"ellipse",
+                    "width":5,
+                    "height":2,
+                    "sprite": 48
+                    },
+                "mass":20
+                },
+            "engorged_tick":{ #Expensive, high dps, fast
+                "max_hp":50,
+                "damage":5,
+                "attack_speed":0.4,
+                "move_speed":3,
+                "shape":{
+                    "type":"ellipse",
+                    "width":5,
+                    "height":2,
+                    "sprite": 48
+                    },
+                "mass":8
+                },
+            "famished_tick":{ #Cheap, super fast, very low mass, decent dps
+                "max_hp":35,
+                "damage":10,
+                "attack_speed":1.5,
+                "move_speed":5,
+                "shape":{
+                    "type":"ellipse",
+                    "width":5,
+                    "height":2,
+                    "sprite": 48
+                    },
+                "mass":2
+                },
+            "foraging_maggot":{ #The worker unit
+                "max_hp":35,
+                "damage":5,
+                "attack_speed":2,
+                "move_speed":2,
+                "shape":{
+                    "type":"ellipse",
+                    "width":5,
+                    "height":2,
+                    "sprite": 48
+                    },
+                "mass":5
+                },
+            "infected_mouse":{ #More expensive better slodier
+                "max_hp":100,
+                "damage":25,
+                "attack_speed":2,
+                "move_speed":2,
+                "shape":{
+                    "type":"ellipse",
+                    "width":5,
+                    "height":2,
+                    "sprite": 48
+                    },
+                "mass":5
+                },
+            "lava_ant":{ #Different soldier ant
+                "max_hp":50,
+                "damage":20,
+                "attack_speed":2,
+                "move_speed":2,
+                "shape":{
+                    "type":"ellipse",
+                    "width":5,
+                    "height":2,
+                    "sprite": 48
+                    },
+                "mass":5
+                },
+            "mantis":{ #Ultimate unit, way too expensive
+                "max_hp":450,
+                "damage":50,
+                "attack_speed":3,
+                "move_speed":1.5,
+                "shape":{
+                    "type":"ellipse",
+                    "width":10,
+                    "height":10,
+                    "sprite": 96
+                    },
+                "mass":15
+                },
+            "mawing_beaver":{ #Tank unit!!
+                "max_hp":250,
+                "damage":5,
+                "attack_speed":1,
+                "move_speed":2.5,
+                "shape":{
+                    "type":"ellipse",
+                    "width":5,
+                    "height":2,
+                    "sprite": 48
+                    },
+                "mass":7
+                },
+            "plagued_bat":{ #expensive, super fast, very low mass, good dps
+                "max_hp":50,
+                "damage":12,
+                "attack_speed":1,
+                "move_speed":5,
+                "shape":{
+                    "type":"ellipse",
+                    "width":5,
+                    "height":2,
+                    "sprite": 48
+                    },
+                "mass":2
+                },
+            "rhino_beetle":{ #Tank, high alpha, super slow attack speed
+                "max_hp":450,
+                "damage":150,
+                "attack_speed":15,
+                "move_speed":1,
+                "shape":{
+                    "type":"ellipse",
+                    "width":5,
+                    "height":4,
+                    "sprite": 48
                     },
                 "mass":10
                 },
-            "mantis":{
-                "max_hp":450,
+            "swooping_bat":{ #expensive, super fast, very low mass, good dps
+                "max_hp":50,
+                "damage":36,
+                "attack_speed":3,
+                "move_speed":5,
                 "shape":{
                     "type":"ellipse",
-                    "width":15,
-                    "height":5
+                    "width":5,
+                    "height":2,
+                    "sprite": 48
                     },
-                "mass":10
-                }
+                "mass":2
+                },
+            "tainted_cockroach":{ #expensive soldier
+                "max_hp":150,
+                "damage":30,
+                "attack_speed":1.75,
+                "move_speed":2,
+                "shape":{
+                    "type":"ellipse",
+                    "width":5,
+                    "height":2,
+                    "sprite": 48
+                    },
+                "mass":7
+                },
+            "tunneling_mole":{ #The inter, high mass soldier
+                "max_hp":50,
+                "damage":15,
+                "attack_speed":2.5,
+                "move_speed":1,
+                "shape":{
+                    "type":"ellipse",
+                    "width":5,
+                    "height":2,
+                    "sprite": 48
+                    },
+                "mass":15
             }
+    }
 
-    game_state = {"units":[]}
+    game_state = {"units":[], "terrain":[]}
 
     for player, units in enumerate(player_units):
         i = 0;
@@ -56,13 +340,17 @@ def main():
             unit["type"] = entity
             unit["player"] = player
             unit["unit_health"] = unit_db[entity]["max_hp"]
+            unit["damage"] = unit_db[entity]["damage"]
+            unit["attack_speed"] = unit_db[entity]["attack_speed"]
+            unit["move_speed"] = unit_db[entity]["move_speed"]
+            unit["mass"] = unit_db[entity]["mass"]
             unit["past_action"] = ""
             unit["lock_out_ticks"] = 0
             if(player == 0):
                 unit["location_x"] = 100
             else:
-                unit["location_x"] = str(map_size[0]-100)
-            unit["location_y"] = str(round((map_size[1] / (len(units)+1)) * (i+1)))
+                unit["location_x"] = map_size[0]-100
+            unit["location_y"] = round((map_size[1] / (len(units)+1)) * (i+1))
             if(player == 0):
                 unit["orientation"] = "right"
             else:
@@ -71,6 +359,8 @@ def main():
             unit["mass"] = unit_db[entity]["mass"]
             game_state["units"].append(unit)
             i+=1
+
+    game_state["terrain"] = generate_terrain(map_size, game_state["units"])
     print(game_state)
 
     return game_state
