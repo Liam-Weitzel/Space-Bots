68f8a284d2a7e884c09fd9b5bfdeb262331e5308
Liam-Weitzel <liam.weitzel@gmail.com>
2024-11-24 21:13:35 +0100

    network refactor & steamworks api integration

---
+++ steamgame/steam.py
@@ -0,0 +1,19 @@
+import ctypes
+import os
+
+current_dir = os.path.dirname(__file__)
+libsteam_api = ctypes.CDLL(os.path.join(current_dir, 'libsteam_api.so'))
+lib = ctypes.CDLL(os.path.join(current_dir, 'SteamworksPy.so'))
+
+from steamworks import STEAMWORKS
+from steamworks.exceptions import SteamNotRunningException
+
+steamworks = STEAMWORKS()
+
+try:
+    steamworks.initialize()
+except SteamNotRunningException:
+    exit()
+
+print(steamworks.Users.GetSteamID())
+print(steamworks.Users.GetAuthSessionTicket())
