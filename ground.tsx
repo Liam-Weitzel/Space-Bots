830e723f09567c6a0efd6a7ffb93e431198ef5ea
Liam-Weitzel <liam.weitzel@gmail.com>
2024-12-10 11:20:24 +0100

    Setting in cave idea

---
+++ ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="ground" tilewidth="16" tileheight="16" tilecount="600" columns="25">
+ <image source="Caverns - Rogue Adventure/Tilesets/RA_Ground_Tiles_Godot.png" width="400" height="384"/>
+</tileset>
