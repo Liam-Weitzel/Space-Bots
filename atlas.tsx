e3b115d083a1a8ef7b2d9b782d642730d133147c
Liam-Weitzel <liam.weitzel@gmail.com>
2024-12-26 18:29:52 +0100

    Add auto tiling with dual grid system

---
+++ atlas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="atlas.tsx" tilewidth="16" tileheight="16" tilecount="16384" columns="128">
+ <image source="atlas.png" width="2048" height="2048"/>
+</tileset>
