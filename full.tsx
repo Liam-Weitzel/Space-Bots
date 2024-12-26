2963fc6d3f29549c7379482c1e247c777535d011
Liam-Weitzel <liam.weitzel@gmail.com>
2024-12-26 18:29:52 +0100

    Add auto tiling with dual grid system

--- full.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="full" tilewidth="16" tileheight="16" tilecount="1792" columns="32">
- <image source="Caverns - Rogue Adventure/Tilesets/RA_Cavern_Full.png" width="512" height="896"/>
-</tileset>
