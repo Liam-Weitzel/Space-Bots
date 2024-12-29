a7c52b43c443cf0764910c4b2249802602ad602f
Liam-Weitzel <liam.weitzel@gmail.com>
2024-12-29 15:12:13 +0100

    Add style switching button & new python base

--- client/assets/atlas4x4.tsx
+++ client/assets/atlas4x4.tsx
@@ -6,10 +6,13 @@
    <object id="1" x="11.5095" y="8.95184" width="41.7219" height="52.9117">
     <ellipse/>
    </object>
-   <object id="5" name="spawn" x="56.7482" y="61.5439">
+   <object id="5" name="spawn_end" x="56.7482" y="61.5439">
     <point/>
    </object>
    <object id="6" name="origin" x="31.4913" y="63.9417">
+    <point/>
+   </object>
+   <object id="8" name="spawn_start" x="26.7813" y="50.3229">
     <point/>
    </object>
   </objectgroup>
@@ -19,10 +22,29 @@
    <object id="1" x="19.1825" y="20.4613" width="40.1234" height="42.2015">
     <ellipse/>
    </object>
-   <object id="2" name="spawn" x="58.8264" y="57.3877">
+   <object id="2" name="spawn_end" x="58.8264" y="57.3877">
     <point/>
    </object>
    <object id="3" name="origin" x="35.9672" y="63.7818">
+    <point/>
+   </object>
+   <object id="4" name="spawn_start" x="29.373" y="42.3317">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="104">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16.0623" y="22.6777" width="34.1245" height="34.9884">
+    <ellipse/>
+   </object>
+   <object id="2" name="origin" x="32.8287" y="63.9295">
+    <point/>
+   </object>
+   <object id="3" name="spawn_end" x="50.7548" y="53.7785">
+    <point/>
+   </object>
+   <object id="4" name="spawn_start" x="27.6452" y="44.9234">
     <point/>
    </object>
   </objectgroup>
