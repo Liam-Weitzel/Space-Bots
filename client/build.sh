ab078e0a5e19ae87a7adc591254b479660bfcb71
Liam-Weitzel <liam.weitzel@gmail.com>
2025-01-06 06:06:01 +0100

    Use git submodules

--- client/build.sh
+++ client/build.sh
@@ -29,15 +29,18 @@ build_shared_lib() {
 }
 
 LIBS_DIR="./libs"
+COMMON_LIBS_DIR="../libs"
 RAYLIB_DIR="$LIBS_DIR/raylib"
 RAYLIB_SRC="$RAYLIB_DIR/src"
-ENTT_DIR="$LIBS_DIR/entt"
-STEAM_DIR="$LIBS_DIR/steam"
-STEAM_LIB_DIR="$STEAM_DIR/linux64"
+RAYGUI_SRC="$LIBS_DIR/raygui/src"
+ENTT_DIR="$COMMON_LIBS_DIR/entt"
+STEAM_DIR="$COMMON_LIBS_DIR/steam/public/steam"
+STEAM_LIB_DIR="$COMMON_LIBS_DIR/steam/redistributable_bin/linux64"
+STEAM_LIB_FILE="$STEAM_LIB_DIR/libsteam_api.so"
 SRC_DIR="./src"
 
 # Common flags
-INCLUDES="-I $RAYLIB_SRC -I $LIBS_DIR -I $ENTT_DIR/src/entt -I $SRC_DIR -I $STEAM_DIR"
+INCLUDES="-I $RAYLIB_SRC -I $RAYGUI_SRC -I $ENTT_DIR/src/entt -I $SRC_DIR -I $STEAM_DIR"
 RAYLIB_LIBS="-L $RAYLIB_SRC -L $RAYLIB_SRC/rtext -lraylib"
 STEAM_LIBS=" -l steam_api -L $STEAM_LIB_DIR"
 SYSTEM_LIBS="-lGL -lm -lpthread -ldl -lrt -lX11"
@@ -50,31 +53,6 @@ if [ "$1" = "hot" ]; then
     exit 0
 fi
 
-# Regular full build path continues here
-mkdir -p $LIBS_DIR
-
-# Clone/download dependencies if not present
-if [ ! -d "$RAYLIB_DIR" ]; then
-    print_status "Cloning raylib..."
-    git clone https://github.com/raysan5/raylib.git $RAYLIB_DIR
-    cd $RAYLIB_DIR
-    git reset --hard 0f6e85a975f637e14b1fed3ff6022a0e0008e620
-    cd ..
-fi
-
-if [ ! -d "$ENTT_DIR" ]; then
-    print_status "Cloning ENTT..."
-    git clone https://github.com/skypjack/entt.git $ENTT_DIR
-    cd $ENTT_DIR
-    git reset --hard 9a19f9aa2f5c7d822ceeea06bce1b0a882c2cb97
-    cd ..
-fi
-
-if [ ! -f "$LIBS_DIR/raygui.h" ]; then
-    print_status "Downloading raygui..."
-    curl -L https://raw.githubusercontent.com/raysan5/raygui/1f813711063f0ff01bd3a89d50ebc17c5412e83c/src/raygui.h -o $LIBS_DIR/raygui.h
-fi
-
 # Build raylib if not built
 if [ ! -f "$RAYLIB_SRC/libraylib.a" ]; then
     print_status "Building raylib..."
@@ -84,7 +62,7 @@ if [ ! -f "$RAYLIB_SRC/libraylib.a" ]; then
 fi
 
 # Copy steamworks library
-cp $STEAM_LIB_DIR/libsteam_api.so ./
+cp $STEAM_LIB_FILE ./
 
 # Build the client
 build_shared_lib
