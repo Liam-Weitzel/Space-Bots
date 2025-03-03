3ea119c41e08346361aed8e8d34a939d61dd3e00
Liam-Weitzel <liam.weitzel@gmail.com>
2025-03-03 07:48:34 +0100

    remove archive, readme, and architecture excalidraw

--- client/src/game_state.h
+++ client/src/game_state.h
@@ -10,6 +10,22 @@ struct GUI {
     int style;
 };
 
+struct RoverAssets {
+    Model body;
+    Vector3 bodyTranlation = {0.0f, 0.5f, 0.0f};
+    Model wheel;
+    Vector3 blWheelTranslation = {0.0f, 0.5f, 0.0f};
+    Vector3 brWheelTranslation = {-2.1f, 0.5f, 0.0f};
+    Vector3 flWheelTranslation = {0.0f, 0.5f, -1.25f};
+    Vector3 frWheelTranslation = {-2.1f, 0.5f, -1.25f};
+    Model scan;
+    Vector3 scanTranslation = {0.0f, 0.5f, 0.0f};
+};
+
+struct ResourceManager {
+    RoverAssets roverAssets;
+};
+
 struct TransientStorage {
     Shader shadowShader;
     RenderTexture2D shadowMap;
@@ -17,9 +33,7 @@ struct TransientStorage {
     int lightDirLoc;
     int lightVPLoc;
     int shadowMapLoc;
-    Mesh roverMesh;
-    Model roverModel;
-    Model cubeModel;
+    ResourceManager resourceManager;
 };
 
 struct GameState {
@@ -31,4 +45,25 @@ struct GameState {
     int frameCount;
     float deltaTime;
     TransientStorage transientStorage;
+};
+
+//*********************
+//  ECS COMPONENTS
+//*********************
+
+struct _Transform {
+    Vector3 translation;
+    Quaternion rotation;
+};
+
+struct _Position {
+    Vector3 position;
+    Quaternion rotation;
+};
+
+struct _Rover {
+    float leftBackWheelRotation;
+    float rightBackWheelRotation;
+    float leftFrontWheelRotation;
+    float rightFrontWheelRotation;
 };
