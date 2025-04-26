abfed4301e302370b9acd3afa21f132cb365b935
Liam-Weitzel <liam.weitzel@gmail.com>
2025-04-26 09:39:15 +0200

    Basic menu/ title screen handling

---
+++ client/src/main_menu.h
@@ -0,0 +1,24 @@
+#pragma once
+
+void DrawMainMenu();
+void RealtimeButton();                // Button: realtimeButton logic
+void ArenaButton();                // Button: arenaButton logic
+void SandboxButton();                // Button: sandboxButton logic
+void CustomGameButton();                // Button: customGameButton logic
+void SettingsButton();                // Button: settingsButton logic
+void PartyUsernameButton4();                // Button: partyUsernameButton4 logic
+void PartyUsernameButton3();                // Button: partyUsernameButton3 logic
+void PartyUsernameButton2();                // Button: partyUsernameButton2 logic
+void PartyUsernameButton1();                // Button: partyUsernameButton1 logic
+void ProfileUsernameButton();                // Button: profileUsernameButton logic
+void ProfileIconButton();                // Button: profileIconButton logic
+void PartyIconButton1();                // Button: partyIconButton1 logic
+void PartyIconButton2();                // Button: partyIconButton2 logic
+void PartyIconButton3();                // Button: partyIconButton3 logic
+void PartyIconButton4();                // Button: partyIconButton4 logic
+void PartyKickButton1();                // Button: partyKickButton1 logic
+void PartyKickButton2();                // Button: partyKickButton2 logic
+void PartyKickButton3();                // Button: partyKickButton3 logic
+void PartyKickButton4();                // Button: partyKickButton4 logic
+void PartyLeaveButton();                // Button: partyLeaveButton logic
+void PartyInviteButton();                // Button: partyInviteButton logic
